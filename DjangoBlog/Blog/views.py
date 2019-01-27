32af9886196e57aa56aa66a78417af47dd2353ac
SeanGoldsmith <goldsmith.sean90@gmail.com>
2019-01-27 14:47:00 -0800

    Added login.html.  Initialized admin check

--- DjangoBlog/Blog/views.py
+++ DjangoBlog/Blog/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from Blog.models import BlogPost
+from django.contrib.auth.decorators import user_passes_test
+
+def admin_check(user):
+    return user.is_superuser
 
 def index(request):
     return HttpResponse('Hello')
+
+@user_passes_test(admin_check)
 def BlogList(request):
     blogList = BlogPost.objects.all()
     template = loader.get_template('blogList.html')

22f43205f5b056ba1e01b8329938c71aab6b370b
SeanGoldsmith <goldsmith.sean90@gmail.com>
2019-01-27 10:08:30 -0800

    Added blogList Template, BlogList view, and urls.

--- DjangoBlog/Blog/urls.py
+++ DjangoBlog/Blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
+    path('BlogList/', views.BlogList, name="BlogList"),
 ]
--- DjangoBlog/Blog/views.py
+++ DjangoBlog/Blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from Blog.models import BlogPost
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hello')
+def BlogList(request):
+    blogList = BlogPost.objects.all()
+    template = loader.get_template('blogList.html')
+    context = {
+        'blogList': blogList,
+    }
+    return HttpResponse(template.render(context, request))

673a138814b490fa827ae6acf3fe58abe8202a43
SeanGoldsmith <goldsmith.sean90@gmail.com>
2019-02-03 11:39:04 -0800

    Updated navbar link to lookup url by name.

--- DjangoBlog/Blog/urls.py
+++ DjangoBlog/Blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('BlogList/', views.BlogList, name="BlogList"),
-    path('PostCreate/', views.PostCreate, name="BlogList"),
+    path('BlogList/', views.BlogList, name="BlogList",),
+    path('PostCreate/', views.PostCreate, name="PostCreate"),
     path('<int:post_id>/Post/', views.Post, name="BlogPost"),
 ]

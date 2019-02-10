ae7a38adcdea4f73890bb69275d3bfc51f441d93
SeanGoldsmith <goldsmith.sean90@gmail.com>
2019-02-10 03:25:27 -0800

    Deleted Home app, adjusted some styling.

--- DjangoBlog/Blog/urls.py
+++ DjangoBlog/Blog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('BlogList/', views.BlogList, name="BlogList",),
+    path('', views.BlogList, name="BlogList",),
     path('PostCreate/', views.PostCreate, name="PostCreate"),
     path('<int:post_id>/Post/', views.Post, name="BlogPost"),
 ]
--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-LOGIN_REDIRECT_URL = '/Blog/'
+LOGIN_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Home',
 ]
 
 MIDDLEWARE = [
